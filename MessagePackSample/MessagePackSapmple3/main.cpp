edbe369f47074ccab3c5fac39ef59aa124ab575d
izumisan <izumi.mite@gmail.com>
2019-03-23 23:56:47 +0900

    add MessagePackSample3

---
+++ MessagePackSample/MessagePackSapmple3/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "msgpack.hpp"
+#include "foo.h"
+
+int main()
+{
+    auto&& foo = Foo();
+    foo.setInt( 7 );
+    foo.setDouble( 777.7777 );
+    foo.setString( "foo" );
+    foo.setVector( { "foo", "bar", "baz" } );
+    foo.setMap(
+    {
+        { "foo", "FOO" },
+        { "bar", "BAR" },
+        { "baz", "BAZ" }
+    } );
+
+    // シリアライズ
+    msgpack::sbuffer buffer;
+    msgpack::pack( buffer, foo );
+
+    std::cout << "sizeof: " << sizeof( foo ) << " buffer: " << buffer.size() << std::endl;
+
+
+    // デシリアライズ
+    msgpack::object_handle handle = msgpack::unpack( buffer.data(), buffer.size() );
+    msgpack::object object = handle.get();
+
+    std::cout << object << std::endl;
+
+    auto&& refoo = Foo();
+    object.convert( refoo );
+
+    std::cout << foo.toString() << std::endl;
+
+
+    std::string buff;
+    std::getline( std::cin, buff );
+
+    return 0;
+}
