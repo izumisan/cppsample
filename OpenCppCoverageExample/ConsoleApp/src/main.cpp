2695616e6ec0b0ed8900b8e7b7dc6bb95d2f2bb8
izumisan <izumi.mite@gmail.com>
2019-12-28 19:26:47 +0900

    Add: ConsoleApp2

--- OpenCppCoverageExample/ConsoleApp/src/main.cpp
+++ OpenCppCoverageExample/ConsoleApp/src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "fizzbuzz.h"
 
 int main()
 {
-    std::cout << "Hello World." << std::endl;
+    std::cout << FizzBuzz::fizzBuzz( 1 ) << std::endl;
+    std::cout << FizzBuzz::fizzBuzz( 2 ) << std::endl;
+    std::cout << FizzBuzz::fizzBuzz( 3 ) << std::endl;
+    std::cout << FizzBuzz::fizzBuzz( 4 ) << std::endl;
+    std::cout << FizzBuzz::fizzBuzz( 5 ) << std::endl;
     return 0;
 }
---
+++ OpenCppCoverageExample/ConsoleApp2/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "fizzbuzz.h"
+
+int main()
+{
+    std::cout << FizzBuzz::fizzBuzz( 15 ) << std::endl;
+    return 0;
+}
