d193f9bcdcef3ccf72b6fa7dca1d9b925ba95802
izumisan <izumi.mite@gmail.com>
2019-12-29 10:49:07 +0900

    Update: runcoverage.ps1

--- OpenCppCoverageExample/runcoverage.ps1
+++ OpenCppCoverageExample/runcoverage.ps1
@@ -1,5 +1,7 @@
+$env:PATH = "OpenCppCoverage-x64;" + "$env:PATH"
+
 # カバレッジをバイナリで出力する
-OpenCppCoverage-x64/OpenCppCoverage.exe --sources=ConsoleApp --sources=Shared --export_type=binary:ConsoleApp.cov -- Debug/ConsoleApp.exe
-OpenCppCoverage-x64/OpenCppCoverage.exe --sources=ConsoleApp2 --sources=Shared --export_type=binary:ConsoleApp2.cov -- Debug/ConsoleApp2.exe
+OpenCppCoverage.exe --sources=ConsoleApp --sources=Shared --export_type=binary:ConsoleApp.cov -- Debug/ConsoleApp.exe
+OpenCppCoverage.exe --sources=ConsoleApp2 --sources=Shared --export_type=binary:ConsoleApp2.cov -- Debug/ConsoleApp2.exe
 # カバレッジレポートのマージ
-OpenCppCoverage-x64/OpenCppCoverage.exe --input_coverage=ConsoleApp.cov --input_coverage=ConsoleApp2.cov
+OpenCppCoverage.exe --input_coverage=ConsoleApp.cov --input_coverage=ConsoleApp2.cov
