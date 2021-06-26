08ca56060e13ee66c245599e4a9dad91c2c829bf
izumisan <izumi.mite@gmail.com>
2021-06-26 23:38:36 +0900

    Update: Requestor

--- NatsSample/RPC/Requestor/main.cpp
+++ NatsSample/RPC/Requestor/main.cpp
@@ -19,13 +19,28 @@ int main()
         std::string input;
         std::getline( std::cin, input );
 
-        const std::string procedureKey( "plusone" );
         for ( int i = 0; i < 10; ++i )
         {
             std::cout << "req: " << i << ", ";
 
+            // Request用関数
+            // - replyキーを指定してpublish
+            //   - natsConnection_PublishRequest()
+            //   - natsConnection_PublishRequestString()
+            // - replyメッセージを待機する
+            //   - natsConnection_Request()
+            //   - natsConnection_RequestString()
+            //   - natsConnection_RequestMsg()
+
             natsMsg* reply = nullptr;
-            s = natsConnection_Request( &reply, connection, procedureKey.c_str(), reinterpret_cast<const void*>( &i ), sizeof( i ), 10000 );
+            if ( i % 2 == 0 )
+            {
+                s = natsConnection_Request( &reply, connection, "plusone", reinterpret_cast<const void*>( &i ), sizeof( i ), 1000 );
+            }
+            else
+            {
+                s = natsConnection_Request( &reply, connection, "plustwo", reinterpret_cast<const void*>( &i ), sizeof( i ), 1000 );
+            }
 
             if ( s != NATS_TIMEOUT )
             {
@@ -42,14 +57,15 @@ int main()
 
             natsMsg_Destroy( reply );
         }
+
+        // NATSサーバーから切断
+        natsConnection_Close( connection );
+        std::cout << "disconnected." << std::endl;
     }
     else
     {
         std::cout << "Fail to connect server!" << std::endl;
     }
-
-    natsConnection_Close( connection );
-    std::cout << "disconnected." << std::endl;
 
     natsConnection_Destroy( connection );
 
