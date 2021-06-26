e1df329ed2673cab470df86033bad6248d4e9919
izumisan <izumi.mite@gmail.com>
2021-06-26 23:38:23 +0900

    Update: Replier

--- NatsSample/RPC/Replier/main.cpp
+++ NatsSample/RPC/Replier/main.cpp
@@ -4,7 +4,7 @@
 #include <nats/nats.h>
 
 static void onPlusOne( natsConnection* connection, natsSubscription* subscription, natsMsg* msg, void* closure );
-
+static void onPlusTwo( natsConnection* connection, natsSubscription* subscription, natsMsg* msg, void* closure );
 static void publish( const std::string& routingKey, const void* bytes, int size );
 
 static natsConnection* connection_ = nullptr;
@@ -20,11 +20,19 @@ int main()
     {
         std::cout << "connected!" << std::endl;
 
-        std::string procedureKey( "plusone" );
-        natsSubscription* subscription = nullptr;
-        volatile bool done = false;
+        // RPCサービス: int plusone(int)
+        natsSubscription* subscription1 = nullptr;
+        {
+            volatile bool done = false;
+            natsConnection_Subscribe( &subscription1, connection_, "plusone", onPlusOne, (void*)( &done ) );
+        }
 
-        natsConnection_Subscribe( &subscription, connection_, procedureKey.c_str(), onPlusOne, (void*)( &done ) );
+        // RPCサービス: int plustwo(int)
+        natsSubscription* subscription2 = nullptr;
+        {
+            volatile bool done = false;
+            natsConnection_Subscribe( &subscription2, connection_, "plustwo", onPlusTwo, (void*)( &done ) );
+        }
 
         std::cout << "running..." << std::endl;
 
@@ -36,7 +44,8 @@ int main()
         natsConnection_Close( connection_ );
         std::cout << "disconnected." << std::endl;
 
-        natsSubscription_Destroy( subscription );
+        natsSubscription_Destroy( subscription1 );
+        natsSubscription_Destroy( subscription2 );
     }
     else
     {
@@ -51,16 +60,36 @@ int main()
 void onPlusOne( natsConnection* connection, natsSubscription* subscription, natsMsg* msg, void* closure )
 {
     auto&& reply = ( natsMsg_GetReply( msg ) != nullptr ) ? std::string( natsMsg_GetReply( msg ) ) : std::string();
-    auto&& data = std::string( natsMsg_GetData( msg ) );
+    auto&& data = natsMsg_GetData( msg );
 
     if ( reply.empty() != true )
     {
-        const int value = *( reinterpret_cast<const int*>( data.c_str() ) );
+        const int value = *( reinterpret_cast<const int*>( data ) );
         int result = value + 1;
 
         publish( reply, &result, sizeof( result ) );
 
-        std::cout << "input: " << value << ", reply: " << result << std::endl;
+        std::cout << "plusone: input: " << value << ", reply: " << result << std::endl;
+    }
+
+    natsMsg_Destroy( msg );
+
+    *(bool*)(closure) = true;
+}
+
+void onPlusTwo( natsConnection* connection, natsSubscription* subscription, natsMsg* msg, void* closure )
+{
+    auto&& reply = ( natsMsg_GetReply( msg ) != nullptr ) ? std::string( natsMsg_GetReply( msg ) ) : std::string();
+    auto&& data = natsMsg_GetData( msg );
+
+    if ( reply.empty() != true )
+    {
+        const int value = *( reinterpret_cast<const int*>( data ) );
+        int result = value + 2;
+
+        publish( reply, &result, sizeof( result ) );
+
+        std::cout << "plustwo: input: " << value << ", reply: " << result << std::endl;
     }
 
     natsMsg_Destroy( msg );
@@ -72,4 +101,3 @@ void publish( const std::string& routingKey, const void* bytes, int size )
 {
     natsConnection_Publish( connection_, routingKey.c_str(), bytes, size );
 }
-
