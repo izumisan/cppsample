82a8d8570b2d71ba87dc83f9c33dd7e07dfad8ed
izumisan <izumi.mite@gmail.com>
2022-08-22 17:32:58 +0900

    Update: Foo.Core.Tests

--- GrpcCliAdapterTrial/GrpcCliAdapterTrial/Foo.Core.Tests/FooServerTest.cs
+++ GrpcCliAdapterTrial/GrpcCliAdapterTrial/Foo.Core.Tests/FooServerTest.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+using System.Diagnostics;
 using NUnit.Framework;
 using Grpc.Core;
 
@@ -17,32 +19,56 @@ namespace Foo.Core.Tests
             string host = "localhost";
             int port = 168080;
 
-            _server = new FooServer( host, port );
+            //@@@ Note:
+            // FooServerをテストプログラムから起動すると、
+            // クライアントからのリクエスト時に"Grpc.Core.RpcExecetionが発生してしまう"
+            // おそらく、同一アプリでサーバー/クライアント同一ポートを占有しているためと思われる...
+            //_server = new FooServer( host, port );
 
             Channel = new Channel( host, port, ChannelCredentials.Insecure );
             Client = new Foo.FooService.FooServiceClient( Channel );
 
+            _server = new Process
+            {
+                StartInfo = new ProcessStartInfo(
+                    Path.Combine(
+                        TestContext.CurrentContext.TestDirectory,
+                        "FooServer.exe" ) )
+                    {
+                        RedirectStandardInput = true,
+                        UseShellExecute = false
+                    }
+            };
             _server.Start();
         }
 
         ~FooServerTest()
         {
-            var s = _server.ShutdownAsync();
-            var c = Channel.ShutdownAsync();
-            Task.WaitAll( new[] { s, c } );
+            //var s = _server.ShutdownAsync();
+            //var c = Channel.ShutdownAsync();
+            //Task.WaitAll( new[] { s, c } );
+
+            //@@@ Note:
+            // FooServer.exeを起動する方法ではテスト自体は成功するが、
+            // デバッガ実行してみると、Channel.ShutdownAsync()で例外が発生している.
+            //
+            // また、FooServerプログラムもkillしているので正常に終了できていない...
+            // (これは、killではなくキー送信してしまえば解決できる気がする)
+            //_server.Kill();
+            _server.StandardInput.WriteLine( "q" );
+            _server.WaitForExit();
+            Channel.ShutdownAsync().Wait();
         }
 
-        private readonly FooServer _server = null;
+        //private readonly FooServer _server = null;
+        private Process _server = null;
         private Channel Channel { get; } = null;
         private Foo.FooService.FooServiceClient Client { get; } = null;
 
         [Test]
-        [Ignore( "Grpc.Core.RpcExecetionが発生してしまう" )]
+        //[Ignore( "Grpc.Core.RpcExecetionが発生してしまう" )]
         public void Sleepテスト()
         {
-            //@@@ Note:
-            // おそらく、同一アプリでサーバー/クライアント同一ポートを占有しているためと思われる...
-
             var actual = Client.Sleep( new SleepRequest { Seconds = 1 } );
             Assert.That( actual.Reply, Is.EqualTo( 1 ) );
         }
